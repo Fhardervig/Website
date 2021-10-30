7d496408fdd159b8f60f6645023b746e5b58941d
Frederik Hardervig <frederik.hardervig@uni.minerva.edu>
2021-10-30 12:33:13 +0100

    Transfer to using Tippy

--- public/main.js
+++ public/main.js
@@ -1,50 +1,9 @@
 $(document).ready(function() {
-    $("#eq1").click(function() {
+    $("#popcorn").click(function() {
         alert($("#eq1tt").html());
     }).css('cursor', 'pointer');
 });
 
-//const popcorn = $('#popcorn');
-//const tooltip = $('#tooltip');
-
-
-
-
-const popcorn = document.querySelector('#popcorn');
-popcorn.setAttribute('aria-describedby', 'tooltip');
-const tooltip = document.querySelector('#tooltip');
-
-// Pass the button, the tooltip, and some options, and Popper will do the
-// magic positioning for you:
-const popperInstance = Popper.createPopper(popcorn, tooltip, {
-    placement: "right",
-    modifiers: [{
-        name: 'offset',
-        options: {
-            offset: [0, 8],
-        },
-    }, ],
-});
-
-function show() {
-    tooltip.setAttribute('data-show', '');
-
-    // We need to tell Popper to update the tooltip position
-    // after we show the tooltip, otherwise it will be incorrect
-    popperInstance.update();
-}
-
-function hide() {
-    tooltip.removeAttribute('data-show');
-}
-
-const showEvents = ['mouseenter', 'focus'];
-const hideEvents = ['mouseleave', 'blur'];
-
-showEvents.forEach((event) => {
-    popcorn.addEventListener(event, show);
-});
-
-hideEvents.forEach((event) => {
-    popcorn.addEventListener(event, hide);
+tippy('#popcorn', {
+    content: "Success!",
 });
