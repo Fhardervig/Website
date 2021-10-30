88138241e99ea0e164c92f76da69e19e9c670e88
Frederik Hardervig <frederik.hardervig@uni.minerva.edu>
2021-10-30 12:32:16 +0100

    Large updates

--- public/main.js
+++ public/main.js
@@ -1,51 +1,50 @@
-const context = {
-  title: 'Welcome to Musicon',
-  body: 'Musicon is a budding musical storefront with a mission to share the joy of music. These magnificent auditory tools are designed with musical creators, like you, in mind. Hobbyists, beginners, and experts alike can appreciate the resplendent sounds supplied by each and every instrument we carry. Join us in delivering the euphoric vibrations of melodia to the citizens of the world!',
-  instruments: [
-    {
-      image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/electronic-keyboard.png',
-      name: 'Electronic Keyboard',
-      description: 'A piano welcomed to the 21th century. Pianists celebrate the compact form factor and the diversity of synthesized rhythms all in one masterful musical machine.',
-      price: '$1,999.00',
-      sale: '$1,699.89'
-    },
-    {
-      image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/electric-guitar.png',
-      name: 'Electric Guitar',
-      description: 'Join the legends of the \'50s and \'60s when the marriage of guitar and electricity created the most influential instrument of a generation. Note: picks sold separately.',
-      price: '$599.99'
-    },
-    {
-      image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/bass-guitar.png',
-      name: 'Bass Guitar',
-      description: 'Experience the embodiment of funkadelic frequencies that is the bass guitar. Let the deep low notes of the bass guitar resonate with heartbeats everywhere.',
-      price: '$624.99'
-    },
-    {
-      image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/drum-kit.png',
-      name: 'Drum Kit',
-      description: 'Ever thought, "one instrument isn\'t enough?" Find an answer in the drum kit. Coordinate a collections of drums and cymbals to dictate the rhythm of musical masterpiece.',
-      price: '$649.00',
-      sale: '$349.00'
-    },
-    {image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/violin.png',
-    name: 'Violin',
-    description: 'A versatile that is suited for any and all occasions. Those wearing tuxedos can strum together a classic. Others who prefer overalls can call it a fiddle and play some folk songs.',
-    price: '$245.00'}
-  ]
-};
+$(document).ready(function() {
+    $("#eq1").click(function() {
+        alert($("#eq1tt").html());
+    }).css('cursor', 'pointer');
+});
 
-const templateElement = document.getElementById('templateHB');
-
-const templateSource = templateElement.innerHTML;
-
-const template = Handlebars.compile(templateSource);
-
-const compiledHtml = template(context);
-
-const targetElement = document.getElementById('information');
-
-targetElement.innerHTML = compiledHtml;
+//const popcorn = $('#popcorn');
+//const tooltip = $('#tooltip');
 
 
 
+
+const popcorn = document.querySelector('#popcorn');
+popcorn.setAttribute('aria-describedby', 'tooltip');
+const tooltip = document.querySelector('#tooltip');
+
+// Pass the button, the tooltip, and some options, and Popper will do the
+// magic positioning for you:
+const popperInstance = Popper.createPopper(popcorn, tooltip, {
+    placement: "right",
+    modifiers: [{
+        name: 'offset',
+        options: {
+            offset: [0, 8],
+        },
+    }, ],
+});
+
+function show() {
+    tooltip.setAttribute('data-show', '');
+
+    // We need to tell Popper to update the tooltip position
+    // after we show the tooltip, otherwise it will be incorrect
+    popperInstance.update();
+}
+
+function hide() {
+    tooltip.removeAttribute('data-show');
+}
+
+const showEvents = ['mouseenter', 'focus'];
+const hideEvents = ['mouseleave', 'blur'];
+
+showEvents.forEach((event) => {
+    popcorn.addEventListener(event, show);
+});
+
+hideEvents.forEach((event) => {
+    popcorn.addEventListener(event, hide);
+});
